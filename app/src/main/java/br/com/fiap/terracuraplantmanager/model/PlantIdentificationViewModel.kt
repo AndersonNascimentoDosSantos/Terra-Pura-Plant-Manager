b87a4a7641ec9bbf94e9423287129ea2a84b147e
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-09-02 01:02:25 -0300

    feat: Maintain state between screens for visualizing plant information.

---
+++ app/src/main/java/br/com/fiap/terracuraplantmanager/model/PlantIdentificationViewModel.kt
@@ -0,0 +1,20 @@
+package br.com.fiap.terracuraplantmanager.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+
+class PlantIdentificationViewModel : ViewModel() {
+    private val _plantInfo = MutableLiveData<JSONObject?>()
+    val plantInfo: LiveData<JSONObject?> = _plantInfo
+
+    fun updatePlantInfo(info: JSONObject?) {
+        viewModelScope.launch(Dispatchers.Main) {
+            _plantInfo.value = info
+        }
+    }
+}
