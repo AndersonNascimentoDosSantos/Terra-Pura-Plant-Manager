34a8af818610ef7aea53156b7949b6c32dd63aed
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-09-11 22:32:40 -0300

    refactor: input data as PlantInfo class and porcentage probability show in the galery like photoshow

--- app/src/main/java/br/com/fiap/terracuraplantmanager/components/imageCard.kt
+++ app/src/main/java/br/com/fiap/terracuraplantmanager/components/imageCard.kt
@@ -14,25 +14,29 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.fiap.terracuraplantmanager.data.PlantInfo
 import coil.compose.rememberImagePainter
 
 @Composable
-fun ImageCard(photoUrl: String, name: String, id: Int, navController: NavController) {
+fun ImageCard(plantInfo: PlantInfo, navController: NavController) {
     Column(
         modifier = Modifier
             .width(150.dp)
             .clickable {
-                navController.navigate("imageDetail/$id")
+                navController.navigate("imageDetail/${plantInfo.index}")
             },
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
-            painter = rememberImagePainter(photoUrl),
+            painter = rememberImagePainter(plantInfo.imageUrl),
             contentDescription = null,
             modifier = Modifier
                 .size(150.dp)
                 .clip(RoundedCornerShape(4.dp))
         )
-        Text(text = name, textAlign = TextAlign.Center)
+        Text(text = plantInfo.name, textAlign = TextAlign.Center)
+        val formattedPercentage = String.format("probabilidade: %.2f%%", plantInfo.probability.toFloat() * 100)
+
+        Text(text = formattedPercentage, textAlign = TextAlign.Center)
     }
 }
