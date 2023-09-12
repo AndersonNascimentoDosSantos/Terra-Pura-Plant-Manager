4035b2898f9af9e6b9995e3c02e21fc7f365fa77
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-09-11 22:34:30 -0300

    refactor: new info added to show probability and conveted index type

--- app/src/main/java/br/com/fiap/terracuraplantmanager/data/PlantInfo.kt
+++ app/src/main/java/br/com/fiap/terracuraplantmanager/data/PlantInfo.kt
@@ -3,5 +3,6 @@ package br.com.fiap.terracuraplantmanager.data
 data class PlantInfo(
     val imageUrl: String,
     val name: String,
-    val index:Int
+    val index: Int,
+    val probability: String
 )
