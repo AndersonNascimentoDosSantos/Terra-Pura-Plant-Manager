132a31a195364359d9851649496f50eb4ea3ba5b
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-09-11 21:44:21 -0300

    refactor: add clickable props and direction to new screen

--- app/src/main/java/br/com/fiap/terracuraplantmanager/components/imageCard.kt
+++ app/src/main/java/br/com/fiap/terracuraplantmanager/components/imageCard.kt
@@ -1,8 +1,11 @@
 package br.com.fiap.terracuraplantmanager.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,35 +13,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 
 @Composable
-fun ImageCard(photoUrl: String, name: String,
-//              onPlantClick: (id: String) -> Unit,
-              id: String
-) {
+fun ImageCard(photoUrl: String, name: String, id: Int, navController: NavController) {
     Column(
-//        modifier = Modifier.clickable { onPlantClick(id) }
+        modifier = Modifier
+            .width(150.dp)
+            .clickable {
+                navController.navigate("imageDetail/$id")
+            },
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
             painter = rememberImagePainter(photoUrl),
             contentDescription = null,
             modifier = Modifier
-                .size(220.dp)
+                .size(150.dp)
                 .clip(RoundedCornerShape(4.dp))
         )
         Text(text = name, textAlign = TextAlign.Center)
     }
-
-
 }
-
-
-//@Composable
-//fun PlantList(suggestions: List<Suggestion>, onPlantClick: (Suggestion) -> Unit) {
-//    LazyRow {
-//        items(suggestions) { suggestion ->
-//
-//        }
-//    }
-//}
